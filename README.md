44bf122de7a5a6fd1339b92bbac80b92faebaf7e
edwin <edwin@lustlab.net>
2018-03-26 12:37:24 +0200

    Renamed Example to TemplateProgram Bumped Gradle wrapper version to 4.6 Added run and jar tasks to build.gradle Added extra instructions to README.md

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# OpenRNDR gradle template
+# OPENRNDR Gradle template
+
+A bare-bones template for writing [OPENRNDR](http://openrndr.org) programs. Contains Gradle project files and a minimal program example.
+
+## Gradle tasks
+
+ * `run` runs the template program (with the correct JVM arguments)
+ * `jar` creates an überjar that contains the program and all its dependencies
+
 
 ## On MacOS
 add `-XstartOnFirstThread` to JVM arguments (at Run/Edit Configurations/VM Options if you're using IntelliJ Idea)
--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -1,14 +1,9 @@
-import org.openrndr.Application
-import org.openrndr.Configuration
-import org.openrndr.Program
+import org.openrndr.*
 import org.openrndr.color.ColorRGBa
 import org.openrndr.math.Vector2
 import org.openrndr.shape.contour
 
-/**
- * This is a basic example that shows how to draw complex shapes made using the contour {} builder
- */
-class Example: Program() {
+class TemplateProgram: Program() {
     override fun draw() {
         drawer.background(ColorRGBa.BLACK)
         drawer.fill = null
@@ -25,5 +20,8 @@ class Example: Program() {
 }
 
 fun main(args: Array<String>) {
-    Application.run(Example(), Configuration())
+    application(TemplateProgram(),
+            configuration {
+
+            })
 }
